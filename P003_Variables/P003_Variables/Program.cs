e8ca4657b4163e3eaa5346b304ea5030bfa990c5
Karolis <karoliens@gmail.com>
2022-06-08 22:11:04 +0300

    Initial commit

---
+++ P003_Variables/P003_Variables/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P003_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string mokyklosPavadinimas = "CodeAcademy";
+            string kursoPavadinimas = ".NET";
+            int studentuSkaicius = 18;
+            DateTime data = DateTime.Now;
+
+            Console.WriteLine($"{mokyklosPavadinimas}\n{kursoPavadinimas}\n{studentuSkaicius}\n{data.ToShortDateString()}");
+
+            */
+
+            /*
+            
+            DateTime kursoPradziosData = new DateTime(2022, 06, 08);
+            DateTime kursoPabaigosData = new DateTime(2022, 12, 12);
+
+            TimeSpan value = kursoPabaigosData - kursoPradziosData;
+
+            Console.WriteLine($"{kursoPradziosData.ToShortDateString()}");
+            Console.WriteLine($"{kursoPabaigosData.ToShortDateString()}");
+            Console.WriteLine($"{value.Days}");
+
+            */
+
+            //Sukurkite tris kintamuosius.tekstinio, sveiko skaitmens ir loginio tipo.
+            //Parašykite programą kuri į konsolę visus aprašytus kintamuosius vienoje eilutėje atskiriant tarpu
+
+            string a = "Ledai";
+            int b = 2;
+            bool asValgau = true;
+        }
+    }
+}
