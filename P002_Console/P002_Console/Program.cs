8779fac0e440db94b3e444a38fd43a3597f8f1c9
Karolis Grybauskas <karoliens@gmail.com>
2022-06-09 23:31:30 +0100

    Update

--- P002_Console/P002_Console/Program.cs
+++ P002_Console/P002_Console/Program.cs
@@ -5,51 +5,89 @@
         static void Main(string[] args)
         {
             //Išveskite į konsolę savo vardą
-
-            //Console.WriteLine("Karolis");
+            
+            Console.WriteLine("Karolis\n");
 
             //Nuskaitykite iš klaviatūros savo vardą ir išveskite į konsolę
 
-            //Console.WriteLine("Įveskite savo vardą:");
-            //Console.WriteLine($"Jūsų vardas: {Console.ReadLine()}");
+            Console.WriteLine("Įveskite savo vardą:");
+            Console.WriteLine($"\nJūsų vardas: {Console.ReadLine()}");
 
             //Nuskaitykite iš klaviatūros savo vardo pirmąją raidę ir išveskite į konsolę jos ASCII kodą
 
-            //Console.WriteLine("Įveskite savo vardo pirmą raidę ir bus išvestas ASCII kodas:");
-            //Console.WriteLine($"\tPirmosios vardo raidės ASCII kodas:{(int)Console.ReadKey().Key}");
+            Console.WriteLine("\nĮveskite savo vardo pirmą raidę ir bus išvestas ASCII kodas:");
+            Console.WriteLine($"\nPirmosios vardo raidės ASCII kodas: {(int)Console.ReadKey().Key}");
 
             //Nuskaitykite iš klaviatūros savo vardo pirmąją raidę, tada nuskaitykite bet kokį skaičių ir išveskite į konsolę sumą. Pabandykite šį skaičių išvesti įvairiais formatais
 
-            Console.WriteLine("Įveskite savo vardo pirmą raidę:");
-            var firstLetter = Console.ReadKey().Key;
-            Console.WriteLine($"\tPirmosios vardo raidės ASCII kodas:{(int)firstLetter}");
+            Console.WriteLine("\nIveskite savo vardo pirmaja raide ir betkoki skaiciu:");
+            Console.WriteLine($"\nRezultatas yra: {(int)Console.ReadKey().KeyChar + (int)Console.ReadKey().KeyChar - 48}");
 
-            Console.WriteLine("Įveskite skaičių:");
-            var anyNumber = Console.ReadLine();
-            Convert.ToInt32(anyNumber);
-            Console.WriteLine($"\tĮvestas skaičius:{anyNumber}");
+            //Padarykite konsolės meniu skirtingose eilutėse(1) Pirkti, (2) Parduoti, (3) Likučiai. Išveskite pasirinktą meniu punktą
 
+            Console.WriteLine("\n(1) Pirkti\n(2) Parduoti\n(3) Likučiai");
+            Console.WriteLine(Console.ReadKey());
 
+            //Parašykite programą, kuri žodžio LABAS kiekvieną raidę išveda naujoje eilutėje
 
+            Console.WriteLine("\nL\nA\nB\nA\nS");
 
-            //Console.WriteLine("(1) Pirkti\n(2) Parduoti\n(3) Likučiai", );
+            //Parašykite programą, kuri žodžio LABAS kiekvieną raidę nutolusią per vieną tab'ą
 
+            Console.WriteLine("\nL\tA\tB\tA\tS");
 
-            //Console.WriteLine("\"LABAS\"");
-            //Console.WriteLine("\u0022LABAS\u0022");
+            //Parašykite programą, kuri žodį LABAS išveda kabutėse.Dviem būdais - naudodami escape ir HEX simbolio kodą
 
+            Console.WriteLine("\n\"LABAS\"");
+            Console.WriteLine("\u0022LABAS\u0022");
 
-            //Console.WriteLine("Karolis");
+            //Parašykite programą, kuri nuskaito jūsų vardą ir išveda antrą raidę
 
+            Console.WriteLine("\nKoks Jusu vardas?");;
+            Console.WriteLine($"Jusu vardo antra raide yra: {Console.ReadLine()[1]}");
 
+            //Parašykite programą, kuri nuskaito jūsų vardą ir išveda raidžių kiekį
 
-            //Console.WriteLine("     " + Console.ReadLine().Length);
-            //Console.WriteLine("Karolis" + Console.ReadLine().Length);
+            Console.WriteLine("\nKoks Jusu vardas?");
+            Console.WriteLine($"Jusu varda sudaro: {Console.ReadLine().Length} raides");
 
-            //Console.WriteLine("\t*********************");
-            //Console.WriteLine("\t**\t\t**");
+            //Parašykite programą kuri nuskaito jūsų vardą ir raidžių kiekį.
 
-            Console.Read(););
+            Console.WriteLine("\nKoks Jusu vardas?");
+            Console.WriteLine($"|\t{Console.ReadLine()} |\t{Console.ReadLine().Length} |");
+
+            //Parašykite programą kuri nuskaito jūsų vardą ir jį išveda 3 eilutėm žemiau(naudoti newline)
+
+            Console.WriteLine("\nKoks Jusu vardas?");
+            Console.WriteLine($"\n\n\n{Console.ReadLine()}");
+
+            //Nupieškiteveiduką
+
+            Console.WriteLine(@"
+           ****************
+        **                  **
+       **    ()       ()      **
+      **          |           **
+     **           |            **
+      **     *    |     *     **
+       **     *        *      **
+        **     * * * *       **
+         **                **
+          ****************");
+
+            //Veiduke visas žvaigždutes pakeiskite dvigubomis kabutėmis
+
+            Console.WriteLine(@"
+           ****************
+        **                  **
+       **    ()       ()      **
+      **          |           **
+     **           |            **
+      **     *    |     *     **
+       **     *        *      **
+        **     * * * *       **
+         **                **
+          ****************".Replace("*", "\""));
         }
     }
 }
--- P003_Variables/P003_Variables/Program.cs
+++ P003_Variables/P003_Variables/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             /*
+            PARAŠYTI PROGRAMĄ KURIOJE SAUGOME:
+            • MOKYKLOS PAVADINIMĄ
+            • KURSO PAVADINIMĄ
+            • STUDENTŲ SKAIČIŲ
+            • ŠIANDIENOS DATĄ
+            • VISUS KINTAMUOSIUS IŠVESTI Į EKRANĄ
+            */
+
             string mokyklosPavadinimas = "CodeAcademy";
             string kursoPavadinimas = ".NET";
             int studentuSkaicius = 18;
@@ -12,27 +20,49 @@
 
             Console.WriteLine($"{mokyklosPavadinimas}\n{kursoPavadinimas}\n{studentuSkaicius}\n{data.ToShortDateString()}");
 
+            /*
+            PAPILDYTI PROGRAMĄ IR PRIDĖTI:
+            • KURSO PRADŽIOS DATĄ
+            • KURSO PABAIGOS DATĄ
+            • Sužinoti skirtumą tarp pradžios ir dabartinės datos (dienomis)
+            • VISUS KINTAMUOSIUS IŠVESTI Į EKRANĄ
             */
 
-            /*
-            
-            DateTime kursoPradziosData = new DateTime(2022, 06, 08);
-            DateTime kursoPabaigosData = new DateTime(2022, 12, 12);
+            DateTime kursoPradziosData = new DateTime(2022, 05, 30);
+            DateTime kursoPabaigosData = new DateTime(2022, 12, 01);
 
-            TimeSpan value = kursoPabaigosData - kursoPradziosData;
+            TimeSpan value = data - kursoPradziosData;
 
             Console.WriteLine($"{kursoPradziosData.ToShortDateString()}");
             Console.WriteLine($"{kursoPabaigosData.ToShortDateString()}");
             Console.WriteLine($"{value.Days}");
 
+            /*
+            Sukurkite tris kintamuosius: tekstinio, sveiko skaitmens ir loginio tipo.
+            Parašykite programą, kuri į konsolę visus aprašytus kintamuosius vienoje eilutėje atskiriant tarpu
             */
 
-            //Sukurkite tris kintamuosius.tekstinio, sveiko skaitmens ir loginio tipo.
-            //Parašykite programą kuri į konsolę visus aprašytus kintamuosius vienoje eilutėje atskiriant tarpu
+            string text = "Ledai";
+            int number = 2;
+            bool boolean = true;
 
-            string a = "Ledai";
-            int b = 2;
-            bool asValgau = true;
+            Console.WriteLine($"{text} {number} {boolean}");
+
+            /*
+            Sukurkite tris sveikojo skaitmens tipo kintamuosius.
+            Parašykite programą kuri į konsolę visus aprašytus kintamuosius atskiriant tarpu
+            - panaudokite konkatinacija
+            - panaudokite kompoziciją
+            - panaudokite interpociacija
+            */
+
+            int number1 = 1;
+            int number2 = 2;
+            int number3 = 3;
+
+            Console.WriteLine(number1 + " " + number2 + " " + number3);
+            Console.WriteLine("{0} {1} {2}", number1, number2, number3);
+            Console.WriteLine($"{ number1} { number2} { number3}");
         }
     }
 }
