f1b255ff96cdeab7b06ed770aa1a3d5b75d89516
Karolis Grybauskas <karoliens@gmail.com>
2022-06-12 00:04:25 +0100

    Update

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -4,137 +4,142 @@
     {
         static void Main(string[] args)
         {
-            /*
-            Nupieškite Tower of Hanoi. Piešiniui naudokite kintamuosius.
-            Apverskite pirmą sulpelį ir išveskite visą Tower of Hanoi
-            Išvalykite pirmą stulpelį ir išveskite tuščią Tower of Hanoi
-            Į kiekvieno stulpelio 5eil įdėkite po 4 dalių elementą ir išveskite Tower of Hanoi
-            Į 1stulp 5eil įdėkite 4 dalių elementą, 2sutup 5eil - 3 dalių, 3sutup 4eil - 1 dalies, 3sutup 5eil - 2 dalių, ir išveskite Tower of Hanoi
-            Į 1stulp 4eil įdėkite tokį pat elementą kaip yra 3stup 4eil, ir išveskite Tower of Hanoi
-            Į visas 2stulp eilutes įdėkite tokį pat elementą kaip yra 3stup 5eil, ir išveskite Tower of Hanoi
-            į 3stulp sudėkite teisingą piramidę. 1stulp ir 2 stulp turi likti tušti, išveskite Tower of Hanoi
-            pakeiskite visų elementų dizainą iš # į " , išveskite Tower of Hanoi
-            paprašykite naudotojo nupiešti 1 sulpelio 1 eilutę. Išveskite visą Tower of Hanoi su perpiešta pirma eilute
-            */
+           //Nupiestas Tower of Hanoi
 
-            //Nupiestas Tower of Hanoi
+            string pirmaEil = "       |     ";
+            string antraEil = "      #|#    ";
+            string treciaEil = "     ##|##   ";
+            string ketvirtaEil = "    ###|###  ";
+            string penktaEil = "   ####|#### ";
+            Console.WriteLine($"1eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"2eil.{antraEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"3eil.{treciaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"4eil.{ketvirtaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"5eil.{penktaEil}{pirmaEil}{pirmaEil}");
 
-            string pirmaEil =    "      |";
-            string antraEil =    "     #|#";
-            string treciaEil =   "    ##|##";
-            string ketvirtaEil = "   ###|###";
-            string penktaEil =   "  ####|####";
-            Console.WriteLine($"1eil.{pirmaEil}\n2eil.{antraEil}\n3eil.{treciaEil}\n4eil.{ketvirtaEil}\n5eil.{penktaEil}");
-            Console.WriteLine("\t----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
             Console.WriteLine("\n\n----testi------");
             Console.ReadLine();
 
             //Nupiestas apverstas Tower of Hanoi
 
-            Console.WriteLine($"\n{penktaEil}\n{ketvirtaEil}\n{treciaEil}\n{antraEil}\n{pirmaEil}");
+            Console.WriteLine($"1eil.{penktaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"2eil.{ketvirtaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"3eil.{treciaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"4eil.{antraEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"5eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
+
+            //Isvalytas Tower of Hanoi
+
+            Console.WriteLine($"1eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"2eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"3eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"4eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"5eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
+
+            //Į kiekvieno stulpelio 5 eilute idetas 4 dalių elementas
+
+            Console.WriteLine($"1eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"2eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"3eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"4eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"5eil.{penktaEil}{penktaEil}{penktaEil}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
 
             /*
-
-            Console.Write($"\n------------------------------------------------------------------------");
-
-            Console.Write($"\n{penktaEil}");
-            Console.Write($"\n{ketvirtaEil}");
-            Console.Write($"\n{treciaEil}");
-            Console.Write($"\n{antraEil}");
-            Console.Write($"\n{pirmaEil}");
-
-            Console.Write($"\n------------------------------------------------------------------------");
-
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-
-            Console.Write($"\n------------------------------------------------------------------------");
-
-            penktaEil = "\t####|####\t\t    ####|####\t\t    ####|####";
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{penktaEil}");
-
-            Console.Write($"\n------------------------------------------------------------------------");
-
-            penktaEil = "\t####|####\t\t     ###|###\t\t      ##|##   ";
-            ketvirtaEil = "\t    |    \t\t        |   \t\t       #|#   ";
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{ketvirtaEil}");
-            Console.Write($"\n{penktaEil}");
-
-            Console.Write($"\n------------------------------------------------------------------------");
-
-            penktaEil = "\t####|####\t\t     ###|###\t\t      ##|##   ";
-            ketvirtaEil = "\t   #|#   \t\t        |   \t\t       #|#   ";
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{ketvirtaEil}");
-            Console.Write($"\n{penktaEil}");
-
-            Console.Write($"\n------------------------------------------------------------------------");
-
-            penktaEil = "\t####|####\t\t      ##|## \t\t      ##|##   ";
-            ketvirtaEil = "\t   #|#   \t\t      ##|## \t\t       #|#   ";
-            pirmaEil = "\t    |    \t\t      ##|## \t\t        |    ";
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{ketvirtaEil}");
-            Console.Write($"\n{penktaEil}");
-
-            Console.Write($"\n------------------------------------------------------------------------");
-
-            pirmaEil = "\t    |\t\t\t        |\t\t\t|";
-            antraEil = "\t    |\t\t\t        |\t\t       #|#";
-            treciaEil = "\t    |\t\t\t        |\t\t      ##|##";
-            ketvirtaEil = "\t    |\t\t\t        |\t\t     ###|###";
-            penktaEil = "\t    |\t\t\t        |\t\t    ####|####";
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{antraEil}");
-            Console.Write($"\n{treciaEil}");
-            Console.Write($"\n{ketvirtaEil}");
-            Console.Write($"\n{penktaEil}");
-
-            Console.Write($"\n------------------------------------------------------------------------");
-
-            pirmaEil = "\t    |\t\t\t        |\t\t\t|";
-            antraEil = "\t    |\t\t\t        |\t\t       #|#";
-            treciaEil = "\t    |\t\t\t        |\t\t      ##|##";
-            ketvirtaEil = "\t    |\t\t\t        |\t\t     ###|###";
-            penktaEil = "\t    |\t\t\t        |\t\t    ####|####";
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{antraEil}".Replace("#", "\""));
-            Console.Write($"\n{treciaEil}".Replace("#", "\""));
-            Console.Write($"\n{ketvirtaEil}".Replace("#", "\""));
-            Console.Write($"\n{penktaEil}".Replace("#", "\""));
-
-            Console.Write($"\n------------------------------------------------------------------------");
-            //paprašykite naudotojo nupiešti 1 sulpelio 1 eilutę.Išveskite visą Tower of Hanoi su perpiešta pirma eilute
-
-            Console.WriteLine("\nĮveskite nuo vieno iki keturių #:");
-            string ivedimas = Console.ReadLine();
-            //Console.WriteLine(ivedimas);
-
-            pirmaEil = $"\t    {ivedimas}|{ivedimas}\t\t\t        |\t\t\t|";
-            antraEil = "\t    |\t\t\t        |\t\t       #|#";
-            treciaEil = "\t    |\t\t\t        |\t\t      ##|##";
-            ketvirtaEil = "\t    |\t\t\t        |\t\t     ###|###";
-            penktaEil = "\t    |\t\t\t        |\t\t    ####|####";
-            Console.Write($"\n{pirmaEil}");
-            Console.Write($"\n{antraEil}".Replace("#", "\""));
-            Console.Write($"\n{treciaEil}".Replace("#", "\""));
-            Console.Write($"\n{ketvirtaEil}".Replace("#", "\""));
-            Console.Write($"\n{penktaEil}".Replace("#", "\""));
+            Į 1 stulpelio 5 eilute įdėtas 4 dalių elementas
+            I 2 sutulpelio 5 eilute idetas 3 dalių elementas
+            I 3 stulpelio 4 eilute idetas 1 dalies elementas
+            I 3 stulpelio 5 eilute idetas 2 dalių elementas
             */
+
+            Console.WriteLine($"1eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"2eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"3eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"4eil.{pirmaEil}{pirmaEil}{antraEil}");
+            Console.WriteLine($"5eil.{penktaEil}{ketvirtaEil}{treciaEil}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
+
+            //Į 1 stulpelio 4 eilute įdėtas toks pat elementas kaip yra 3 stulpelyje 4 eiluteje
+
+            Console.WriteLine($"1eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"2eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"3eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"4eil.{antraEil}{pirmaEil}{antraEil}");
+            Console.WriteLine($"5eil.{penktaEil}{ketvirtaEil}{treciaEil}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
+
+            //Į visas 2 stulpelio eilutes įdėtas toks pat elementas kaip yra 3 stulpelio 5 eiluteje
+
+            Console.WriteLine($"1eil.{pirmaEil}{antraEil}{pirmaEil}");
+            Console.WriteLine($"2eil.{pirmaEil}{antraEil}{pirmaEil}");
+            Console.WriteLine($"3eil.{pirmaEil}{antraEil}{pirmaEil}");
+            Console.WriteLine($"4eil.{antraEil}{antraEil}{antraEil}");
+            Console.WriteLine($"5eil.{penktaEil}{antraEil}{treciaEil}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
+
+            /*
+            I 3 stulpeli sudėta teisinga piramide
+            1 stulpelis ir 2 stulpelis turi likti tušti
+            */
+
+            Console.WriteLine($"1eil.{pirmaEil}{pirmaEil}{pirmaEil}");
+            Console.WriteLine($"2eil.{pirmaEil}{pirmaEil}{antraEil}");
+            Console.WriteLine($"3eil.{pirmaEil}{pirmaEil}{treciaEil}");
+            Console.WriteLine($"4eil.{pirmaEil}{pirmaEil}{ketvirtaEil}");
+            Console.WriteLine($"5eil.{pirmaEil}{pirmaEil}{penktaEil}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
+
+            //pakeiskitas visų elementų dizainas iš # į "
+
+            Console.WriteLine($"1eil.{pirmaEil}{pirmaEil}{pirmaEil.Replace("#","\"")}");
+            Console.WriteLine($"2eil.{pirmaEil}{pirmaEil}{antraEil.Replace("#", "\"")}");
+            Console.WriteLine($"3eil.{pirmaEil}{pirmaEil}{treciaEil.Replace("#", "\"")}");
+            Console.WriteLine($"4eil.{pirmaEil}{pirmaEil}{ketvirtaEil.Replace("#", "\"")}");
+            Console.WriteLine($"5eil.{pirmaEil}{pirmaEil}{penktaEil.Replace("#", "\"")}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
+
+            //paprašykite naudotojo nupiešti 1 sulpelio 1 eilutę. Išveskite visą Tower of Hanoi su perpiešta pirma eilute
+
+            Console.WriteLine("Nupieskite 1 stulpelio 1 eilute");
+
+            string ivedimas = Console.ReadLine();
+
+            Console.WriteLine($"1eil.{ivedimas}{pirmaEil}{pirmaEil.Replace("#", "\"")}");
+            Console.WriteLine($"2eil.{pirmaEil}{pirmaEil}{antraEil.Replace("#", "\"")}");
+            Console.WriteLine($"3eil.{pirmaEil}{pirmaEil}{treciaEil.Replace("#", "\"")}");
+            Console.WriteLine($"4eil.{pirmaEil}{pirmaEil}{ketvirtaEil.Replace("#", "\"")}");
+            Console.WriteLine($"5eil.{pirmaEil}{pirmaEil}{penktaEil.Replace("#", "\"")}");
+
+            Console.WriteLine("      ----1stulp-------2stulp-------3stulp----");
+            Console.WriteLine("\n\n----testi------");
+            Console.ReadLine();
         }
     }
 }
