b6e503371e82ab6b7c3647c4df996f11313c2a29
Karolis <karoliens@gmail.com>
2022-06-14 22:44:36 +0300

    Initial Commit

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Paprašykite naudotojo įvesti 1 skaičių - temperatūrą pagal Celsijų. 
+            - Paskaičiuokite ir išveskite į ekraną temperatūrą pagal farenheitą.
+            - Paskaičiuokite ir išveskite į ekraną temperatūrą pagal kelviną.
+            - Gautą temperatūrą pagal farenheitą perskaičiuokite į Celsijų ir patikrinkite ar sutampa su įvestu skaičių (išveskite true/false)
+            - Gautą temperatūrą pagal kelviną perskaičiuokite į celsijų ir patikrinkite ar sutampa su įvestu skaičiu (išveskite true/false)
+            - Paskaičiuotą temperatūrą pagal farenheita peverskite į kelviną ir patikrinkite ar sutampa su ankstesniais skaičiavimais (išveskite true/false)
+            - Nupieškite termometrą pagal Celsijų 
+            a) Atvaizduokite skalę, sugraduotą kas 5 laipsnius C priklausomai nuo įvestos temperatūros pridedant ir atimant 40 laipsnių 
+            (tarkime įvesta buvo 10, tuomet skalė bus nuo -30 iki +50)
+            b) Grafiškai atvaizduokite įvestą temperatūros stulpelį. 
+            <HINT> naudokite .ToString(), palyginimo reliacinius operatorius (==, >, < ir t.t.) ir .Replace(). 
+            if naudoti negalima, ternary operator (?) naudoti negalima.
+            rezultatas gali atrodyti taip:
+                            |--------------------|
+                            |   ^F     _    ^C   |
+                            |  100  - | | -  40  |
+                            |   95  - | | -  35  |
+                            |   90  - | | -  30  |
+                            |   80  - | | -  25  |
+                            |   70  - | | -  20  |
+                            |   60  - | | -  15  |
+                            |   50  - |#| -  10  |
+                            |   40  - |#| -   5  |
+                            |   30  - |#| -   0  |
+                            |   20  - |#| -  -5  |
+                            |   10  - |#| - -10  |
+                            |    5  - |#| - -15  |
+                            |    0  - |#| - -20  |
+                            |  -10  - |#| - -25  |
+                            |  -20  - |#| - -30  |
+                            |  -30  - |#| - -35  |
+                            |  -40  - |#| - -40  |
+                            |        '***`       |
+                            |       (*****)      |
+                            |        `---'       |
+                            |____________________|
+ */
+            Console.WriteLine("Įveskite temperatūrą pagal Celsijų:");
+            var tempC = double.Parse(Console.ReadLine());
+            var tempF = tempC * 1.8 + 32;
+            var tempK = tempC + 273.15;
+
+            Console.WriteLine($"Temperatūra pagal Farenheitą: {tempF}");
+
+            Console.WriteLine($"Temperatūra pagal Kelviną: {tempK}");
+
+            var tempCPerskaiciuotaIsF = (tempF - 32) / 1.8;
+            //Console.WriteLine(tempFPerskaiciuota);
+            Console.WriteLine($"Ar perskaičiuota temperatūra pagal Farenheitą sutampa su įvestu skaičiu? {tempCPerskaiciuotaIsF == tempC}");
+
+            var tempCPerskaiciuotaIsK = tempK - 273.15;
+            Console.WriteLine($"Ar perskaičiuota temperatūra pagal Celcijų sutampa su įvestu skaičiu? {tempCPerskaiciuotaIsK == tempC}");
+
+            var tempKPerskaiciuotaIsF = (tempF - 32) * 5 / 9 + 273.15;
+            Console.WriteLine($"Ar perskaičiuota temperatūra pagal Kelviną sutampa su ankstesniu skaičiavimu? {tempKPerskaiciuotaIsF == tempK}");
+
+            
+
+            var skaleMax = tempC + 40;
+            var skaleMin = tempC - 40;
+
+            Console.WriteLine(skaleMin);
+            Console.WriteLine(skaleMax);
+
+            Console.WriteLine("|--------------------|");
+            Console.WriteLine("|   ^F     _    ^C   |");
+            Console.WriteLine($" {skaleMax}");
+            Console.WriteLine($" {skaleMax - 5}");
+            Console.WriteLine($" {skaleMax - 10}");
+            Console.WriteLine($" {skaleMax - 15}");
+            Console.WriteLine($" {skaleMax - 20}");
+            Console.WriteLine($" {skaleMax - 25}");
+            Console.WriteLine($" {skaleMax - 30}");
+            Console.WriteLine($" {skaleMax - 35}");
+            Console.WriteLine($" {skaleMax - 40}");
+            Console.WriteLine($" {skaleMax - 45}");
+            Console.WriteLine($" {skaleMax - 50}");
+            Console.WriteLine($" {skaleMax - 55}");
+            Console.WriteLine($" {skaleMax - 60}");
+            Console.WriteLine($" {skaleMax - 65}");
+            Console.WriteLine($" {skaleMax - 70}");
+            Console.WriteLine($" {skaleMax - 75}");
+            Console.WriteLine($" {skaleMax - 80}");
+            Console.WriteLine($" {skaleMin}");
+
+        }
+    }
+}
--- P004_CastingAndTypeConversions/P004_CastingAndTypeConversions/Program.cs
+++ P004_CastingAndTypeConversions/P004_CastingAndTypeConversions/Program.cs
@@ -11,7 +11,7 @@
             • SANDAUGĄ
             • DALYBĄ
             */
-
+            /*
             Console.WriteLine("Įveskite pirmą skaičių:");
             string skaicius1 = Console.ReadLine();
 
@@ -23,16 +23,13 @@
             Console.WriteLine($"Skaičių sandauga: {Convert.ToInt32(skaicius1) * Convert.ToInt32(skaicius2)}");
             Console.WriteLine($"Skaičių dalyba: {Convert.ToInt32(skaicius1) / Convert.ToInt32(skaicius2)}");
 
-            
             //PARAŠYTI PROGRAMĄ, KURIOJE VARTOTOJO PRAŠOMA ĮVESTI 3 SKAIČIUS. PROGRAMA TURI IŠVESTI ŠIŲ SKAIČIŲ VIDURKĮ.
             
-
             Console.WriteLine("Įveskite pirmą skaičių:");
             int s1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Įveskite antrą skaičių:");
             int s2 = int.Parse(Console.ReadLine());
-
 
             Console.WriteLine("Įveskite trečią skaičių:");
             int s3 = int.Parse(Console.ReadLine());
@@ -46,6 +43,120 @@
             - iš rezultato atimkite maksimalų long skaičių
             - ir pridėkite maksimalų int skaičių
             */
+            /*
+            long ilgas = long.MaxValue;
+            short trumpas = short.MaxValue;
+
+            Console.WriteLine($"Dalyba: {(double)ilgas / trumpas}");
+
+            */
+
+            /*
+            PARAŠYTI PROGRAMĄ KURI DIDELĮ 10 ŽENKLĮ SKAIČIŲ DOUBLE, KONVERTUOJA Į
+            INT , SHORT , BYTE
+            STEBĖTI REZULTATĄ.
+            */
+
+            /*
+
+            double didelisSkaicius = 1000000000;
+
+            Console.WriteLine($"INT: {Convert.ToInt32(didelisSkaicius)}");
+            Console.WriteLine($"SHORT: {Convert.ToInt16(didelisSkaicius)}");
+            Console.WriteLine($"BYTE: {Convert.ToByte(didelisSkaicius)}");
+            
+            */
+
+            /*
+            PARAŠYTI PROGRAMĄ KURI
+            PRAŠO ĮVESTI RUTULIO DIAMETRĄ,
+            O IŠVEDA PLOTĄ IR TŪRĮ
+
+            */
+            /*
+            Console.WriteLine("Įveskite rutulio diametrą:");
+            var d = Console.ReadLine();
+
+
+
+            Console.WriteLine($"Plotas: {4 * 3.14 * Convert.ToInt16(d) * Convert.ToInt16(d)}");
+            Console.WriteLine($"Tūris: {4 / 3 * 3.14 * Convert.ToInt16(d) * Convert.ToInt16(d) * Convert.ToInt16(d)}");
+            */
+
+            /*
+            PARAŠYTI PROGRAMĄ KURI PRAŠO ĮVESTI ATSTUMĄ (METRAIS) IR LAIKĄ (SEKUNDĖMIS),
+            - IŠVEDA GREITĮ km/h.
+            - IŠVEDA GREITĮ km/s.
+            */
+
+
+            /*
+            Console.WriteLine("Įveskite atstumą metrais:");
+            var atstumas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Įveskite laiką sekundėmis");
+            var laikas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Greitis km/h: {((atstumas) / 1000) / ((laikas) * 3600)}");
+            Console.WriteLine($"Greitis km/s: {((atstumas) / 1000) / (laikas)}");
+
+            */
+
+            /*
+            
+            */
+            /*
+            var x = int.Parse(Console.ReadLine());
+            var y = int.Parse(Console.ReadLine());
+            var rezultatas1 = y + 2 * y + x + 1;
+            var rezultatas2 = y * y + x / 2;
+
+            Console.WriteLine($"{rezultatas1}");
+            Console.WriteLine($"{rezultatas2}");
+            */
+
+            /*
+            PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI PENKIAŽENKLĮ SVEIKĄ SKAIČIŲ
+            VISUS ĮVESTUS 1 PAKEISKITE Į 0 IR GAUTĄ SKAIČIŲ PADALINKITE IŠ 2
+            PVZ BUVO ĮVESTA 12345
+            REZULTATAS 2345/2=1172,5
+            <HINT> naudokite replace
+            */
+
+            /*
+            Console.WriteLine("Įveskite penkiaženklį sveiką skaičių:");
+
+            int skaicius = int.Parse(Console.ReadLine().Replace("1", "0"));
+            Console.WriteLine($"{skaicius}");
+            */
+
+            /*
+            PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI PENKIAŽENKLĮ SVEIKĄ SKAIČIŲ
+            VISUS ĮVESTUS išskyrus 1 PAKEISKITE Į 0 IR GAUTĄ SKAIČIŲ PADALINKITE IŠ 2
+            PVZ BUVO ĮVESTA 12345
+            REZULTATAS 10000/2=5000
+            <HINT> naudokite replace
+            */
+            /*
+            Console.WriteLine("Įveskite penkiaženklį sveiką skaičių:");
+            int skaicius = int.Parse(Console.ReadLine().Replace("2", "0", "1"));
+            */
+
+            /*
+            PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI SVEIKĄ SKAIČIŲ
+            VIENOJE EILUTĖJE IŠVESKITE ŠĮ SKAIČIŲ 5 KARTUS VIS PADIDINAMI VIENETU.
+            PVZ BUVO ĮVESTA 5
+            REZULTATAS 6 7 8 9 10
+            */
+
+            Console.WriteLine("Įveskite sveiką skaičių:");
+            var skaicius = int.Parse(Console.ReadLine());
+            var skaiciusPadidintas = ++skaicius;
+
+            Console.WriteLine($"{skaiciusPadidintas}{skaiciusPadidintas+1}{skaiciusPadidintas+2}{skaiciusPadidintas+3}{skaiciusPadidintas+4}");
+
+
+
         }
     }
 }
