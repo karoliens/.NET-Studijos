4df998bc1b9de8befc7c98300edfc7d1b9382aa0
Karolis <karoliens@gmail.com>
2022-06-23 18:35:05 +0300

    Add solutions for task 1 and 2

--- P007_Conditions/P007_Conditions/Program.cs
+++ P007_Conditions/P007_Conditions/Program.cs
@@ -4,50 +4,116 @@
     {
         static void Main(string[] args)
         {
-         /*
-         PARAŠYTI PROGRAMĄ, KURI PAPRAŠO VARTOTOJO ĮVESTI
-         EGZAMINO PAŽYMĮ. IŠVESTI:
-         0 - 4: NEPATEKINAMAI
-         5: SILPNAI
-         6: PATENKINAMAI
-         7: VIDUTINIŠKAI
-         8: GERAI
-         9: LABAI GERAI
-         10: PUIKIAI
-         */
+            /*
+            PARAŠYTI PROGRAMĄ, KURI PAPRAŠO VARTOTOJO ĮVESTI
+            EGZAMINO PAŽYMĮ. IŠVESTI:
+            0 - 4: NEPATEKINAMAI
+            5: SILPNAI
+            6: PATENKINAMAI
+            7: VIDUTINIŠKAI
+            8: GERAI
+            9: LABAI GERAI
+            10: PUIKIAI
+            */
+            
+            Console.WriteLine("Įveskite egzamino pažymį:");
+            var pazymys = int.Parse(Console.ReadLine());
 
-         /*
-         ** Nemokama kava **
-         Aprašykite int kintamajį kuriame nurodysite kiek puodelių kavos perkama. (tarkim 7)
-         Kas 3 puodelis nemokamas. Paskaičiuokite ir išveskite į ekraną ar pirkėjui priklauso nemokama kava
-         - Pavyzdžio atsakymas: "pirkėjui priklauso 2 nemokami puodeliai"
-         - Alternatyvus atsakymas: "Pirkėjui nepriklauso nemokama kava"
-         */
+            if (pazymys < 0)
+            {
+                Console.WriteLine("KLAIDA");
+            }
 
-         /*
-         ** Piešingi skaičiai **
-         Naudotojas įveda betkokius 4 sveikus skaicius (tarkim 5; 15; -25; 0;)
-         - Parašykite programą kuri į ekraną išves neigiamą/teigiamą skaičiaus reikšmę
-         - Pavyzdžio atsakymas 5 -> -5; 15 -> -15; -25 -> 25; 0 -> N/A;
-         */
+            else if (pazymys == 5)
+            {
+                Console.WriteLine("SILPNAI");
+            }
 
-         /*
-         ŽAIDIMAS ATSPĖK SKAIČIŲ
-         - ĮHARDKODINAMAS BETKOKS SKAIČIUS NUO 1 IKI 20
-         - NAUDOTOJAS 6 KARTUS PRAŠOMAS ĮVESTI SKAIČIŲ NUO 1 IKI 20
-         - JEI NAUDOTOJAS ĮVEDĖ TEISINGAI - IŠVEDAMAS SVEIKINIMAS "TEISINGAI" IR PROGRAMA STABDOMA
-         - JEI NAUDOTOJAS ĮVEDĖ PER MAŽĄ SKAIČIŲ - IŠVEDAMAS PRANEŠIMAS "SKAIČIUS YRA DIDESNIS"
-         - JEI NAUDOTOJAS ĮVEDĖ PER DIDELĮ SKAIČIŲ - IŠVEDAMAS PRANEŠIMAS "SKAIČIUS YRA MAŽESNIS"
-         nutraukiant programos vykdymą Environment.Exit(0) ar pan. naudoti negalima. Naudoti if.
-         */
+            else if (pazymys == 6)
+            {
+                Console.WriteLine("PATENKINAMAI");
+            }
 
-         /*
-         * SKAIČIUOTUVAS *
-         Paprašykite naudotojo įvesti du skaičius ir matematinę operaciją ( +, -, * arba / )
-         - Parašykite programą kuri į ekraną išves skaičių rezultatą
-         - naudokite if
-         - naudokite switch
-         */
+            else if (pazymys == 7)
+            {
+                Console.WriteLine("VIDUTINIŠKAI");
+            }
+
+            else if (pazymys == 8)
+            {
+                Console.WriteLine("GERAI");
+            }
+
+            else if (pazymys == 9)
+            {
+                Console.WriteLine("LABAI GERAI");
+            }
+
+            else if (pazymys == 10)
+            {
+                Console.WriteLine("PUIKIAI");
+            }
+
+            else
+            {
+                Console.WriteLine("NEPATENKINAMAI");
+            }
+
+            /*
+            ** Nemokama kava **
+            Aprašykite int kintamajį kuriame nurodysite kiek puodelių kavos perkama. (tarkim 7)
+            Kas 3 puodelis nemokamas. Paskaičiuokite ir išveskite į ekraną ar pirkėjui priklauso nemokama kava
+            - Pavyzdžio atsakymas: "pirkėjui priklauso 2 nemokami puodeliai"
+            - Alternatyvus atsakymas: "Pirkėjui nepriklauso nemokama kava"
+            */
+
+            var puodeliuKiekis = -2;
+            var nemokamiPuodeliai = puodeliuKiekis / 3;
+
+            if (nemokamiPuodeliai == 1)
+            {
+                Console.WriteLine($"Pirkėjui priklauso {nemokamiPuodeliai} nemokamas puodelis");
+            }
+
+            else if (nemokamiPuodeliai > 1)
+            {
+                Console.WriteLine($"Pirkėjui priklauso {nemokamiPuodeliai} nemokami puodeliai");
+            }
+
+            else if (nemokamiPuodeliai <= 0)
+            {
+                Console.WriteLine("KLAIDA");
+            }
+
+            else
+            {
+                Console.WriteLine("Pirkėjui nepriklauso nemokami puodeliai");
+            }
+
+            /*
+            ** Piešingi skaičiai **
+            Naudotojas įveda betkokius 4 sveikus skaicius (tarkim 5; 15; -25; 0;)
+            - Parašykite programą kuri į ekraną išves neigiamą/teigiamą skaičiaus reikšmę
+            - Pavyzdžio atsakymas 5 -> -5; 15 -> -15; -25 -> 25; 0 -> N/A;
+            */
+
+            /*
+            ŽAIDIMAS ATSPĖK SKAIČIŲ
+            - ĮHARDKODINAMAS BETKOKS SKAIČIUS NUO 1 IKI 20
+            - NAUDOTOJAS 6 KARTUS PRAŠOMAS ĮVESTI SKAIČIŲ NUO 1 IKI 20
+            - JEI NAUDOTOJAS ĮVEDĖ TEISINGAI - IŠVEDAMAS SVEIKINIMAS "TEISINGAI" IR PROGRAMA STABDOMA
+            - JEI NAUDOTOJAS ĮVEDĖ PER MAŽĄ SKAIČIŲ - IŠVEDAMAS PRANEŠIMAS "SKAIČIUS YRA DIDESNIS"
+            - JEI NAUDOTOJAS ĮVEDĖ PER DIDELĮ SKAIČIŲ - IŠVEDAMAS PRANEŠIMAS "SKAIČIUS YRA MAŽESNIS"
+            nutraukiant programos vykdymą Environment.Exit(0) ar pan. naudoti negalima. Naudoti if.
+            */
+
+            /*
+            * SKAIČIUOTUVAS *
+            Paprašykite naudotojo įvesti du skaičius ir matematinę operaciją ( +, -, * arba / )
+            - Parašykite programą kuri į ekraną išves skaičių rezultatą
+            - naudokite if
+            - naudokite switch
+            */
         }
     }
 }
