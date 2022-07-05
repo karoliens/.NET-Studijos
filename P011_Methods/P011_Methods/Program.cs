597459d9c2be6941ea8f195912fb64961f9bbd29
Karolis <karoliens@gmail.com>
2022-07-06 00:46:44 +0300

    Initial commit

--- P011_Methods/P011_Methods/Program.cs
+++ P011_Methods/P011_Methods/Program.cs
@@ -8,19 +8,38 @@
             var pirmasSkaicius = int.Parse(Console.ReadLine());
             var antrasSkaicius = int.Parse(Console.ReadLine());
 
+            Menu(pirmasSkaicius, antrasSkaicius);
+
+            //var menuPasirinkimas = Console.ReadLine();
+
+            //if ()
+
+
+
+
+
+            /*
             Console.WriteLine($"Sudėtis: {Suma(pirmasSkaicius, antrasSkaicius)}");
             Console.WriteLine($"Atimtis: {Atimtis(pirmasSkaicius, antrasSkaicius)}");
             Console.WriteLine($"Daugyba: {Daugyba(pirmasSkaicius, antrasSkaicius)}");
             Console.WriteLine($"Dalyba: {Dalyba(pirmasSkaicius, antrasSkaicius)}");
 
             Console.WriteLine($"Visų gautų rezultatų suma: {Suma(pirmasSkaicius, antrasSkaicius) + Atimtis(pirmasSkaicius, antrasSkaicius) + Daugyba(pirmasSkaicius, antrasSkaicius) + Dalyba(pirmasSkaicius, antrasSkaicius)}");
-
+            
             Console.WriteLine("Įveskite du skaičius ir matematninį veiksmą:");
             var pirmasSkaicius1 = int.Parse(Console.ReadLine());
             var antrasSkaicius1 = int.Parse(Console.ReadLine());
             var matematinisVeiksmas = Console.ReadLine();
 
             Console.WriteLine(Skaiciuotuvas(pirmasSkaicius1, antrasSkaicius1, matematinisVeiksmas));
+
+            Console.WriteLine("Įveskite taisiklingojo daugiakampio kraštinių kiekį (n):");
+            var krastiniuKiekis = int.Parse(Console.ReadLine());
+            Console.WriteLine("Įveskite taisiklingojo daugiakampio kraštinės ilgį (b):");
+            var krastiniuIlgis = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Plotas: {PlotoSkaiciavimas(krastiniuKiekis, krastiniuIlgis)}");
+            Console.WriteLine($"Vidinių kampų suma: {VidiniuKampuSuma(krastiniuKiekis)}");*/
         }
 
         /*
@@ -94,6 +113,158 @@
         visi atvejai 1,2,3 ir 4 turi būti atskiruose metoduose ir metodai turi būti testuojami
         */
 
+        public static bool IsTrikampis(int krastiniuKiekis)
+        {
+            if (krastiniuKiekis == 3)
+                return true;
+            else
+                return false;
+        }
 
+        public static bool IsKeturkampis(int krastiniuKiekis)
+        {
+            if (krastiniuKiekis == 4)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsVisaKita(int krastiniuKiekis)
+        {
+            if (krastiniuKiekis > 4)
+                return true;
+            else
+                return false;
+        }
+
+        public static int VidiniuKampuSuma(int krastiniuKiekis)
+        {
+            return 180 * (krastiniuKiekis - 2);
+        }
+
+        public static double PlotoSkaiciavimas(int krastiniuKiekis, int krastiniuIlgis)
+        {
+            if (IsTrikampis(krastiniuKiekis))
+                Console.WriteLine("Įveskite aukštį (h):");
+                var aukstis = Console.ReadLine();
+                    return (1 / (double)2) * krastiniuIlgis * krastiniuKiekis;
+
+            if (IsKeturkampis(krastiniuKiekis))
+                    return krastiniuIlgis * krastiniuIlgis;
+
+            if (IsVisaKita(krastiniuKiekis))
+                Console.WriteLine("Įveskite statmenį (r):");
+                var statmuo = int.Parse(Console.ReadLine());
+                    return (krastiniuKiekis / (double)2) * krastiniuIlgis * statmuo;
+   
+                return 0;
+        }
+
+        /*
+        1.Naudodami method overloading sukurkite metodus Suma, Atimtis, Daugyba, Dalyba kurie priima du double tipo parametrus.
+        (prieštai sukurtų metodų ištrinti negalima)
+
+        2. Naudotojui įvedus skaičius nustatykite ar buvo įvestas skaičius su kableliu ar be ir duomenis nukreipkite reikiamiems metodams. 
+        (Informaciją apie tai, koks metodas buvo panaudotas išveskite į debug konsolę)
+
+        3. Matematinius metodus palildykite kėlimu kvadratu (^2) ir kėlimu kūbu ( ^3).
+
+        4.Padarykite meniu, kur naudotojui leis pasirinkti koks matematinis veiksmas bus atliekamas 
+        (gali parinkti arba veiksmą, arba veiksmo numerį meniu. Abiem atvejais bus atliekama matematinė operacija) 
+        (Pasirinkimams panaudoti switch sakinį)
+          1) +
+          2) -
+          3) *
+          4) /
+          5) a^2
+          6) a^3
+        */
+
+        public static double Suma(double pirmasSkaicius, double antrasSkaicius)
+        {
+            return pirmasSkaicius + antrasSkaicius;
+        }
+
+        public static double Atimtis(double pirmasSkaicius, double antrasSkaicius)
+        {
+            return pirmasSkaicius - antrasSkaicius;
+        }
+
+        public static double Daugyba(double pirmasSkaicius, double antrasSkaicius)
+        {
+            return pirmasSkaicius * antrasSkaicius;
+        }
+
+        public static double Dalyba(double pirmasSkaicius, double antrasSkaicius)
+        {
+            return pirmasSkaicius / antrasSkaicius;
+        }
+
+        public static double KelimasKvadratu(double pirmasSkaicius)
+        {
+            return pirmasSkaicius * pirmasSkaicius;
+        }
+
+        public static double KelimasKubu(double pirmasSkaicius)
+        {
+            return pirmasSkaicius * pirmasSkaicius * pirmasSkaicius;
+        }
+
+        public static bool YraKablelis(string pirmasSkaicius, string antrasSkaicius)
+        {
+            if (pirmasSkaicius.Contains(",") && antrasSkaicius.Contains(","));
+                return true;
+            
+            return
+                    false;
+        }
+
+        public static void Menu(int pirmasSkaicius, int antrasSkaicius)
+        {
+            Console.WriteLine("Pasirinkite matematinį veiksmą:");
+            var pasirinkimas = Console.ReadLine();
+
+            var menuPasirinkimas = pasirinkimas switch
+            {
+                "1)" or "+" => Suma(pirmasSkaicius, antrasSkaicius),
+                "2)" or "-" => Atimtis(pirmasSkaicius, antrasSkaicius),
+                "3)" or "*" => Daugyba(pirmasSkaicius, antrasSkaicius),
+                "4)" or "/" => Dalyba(pirmasSkaicius, antrasSkaicius),
+                "5)" or "a^2" => KelimasKvadratu(pirmasSkaicius),
+                "6)" or "a^3" => KelimasKubu(pirmasSkaicius),
+            };
+
+            //return menuPasirinkimas;
+        }
+
+        /*
+        Tarkime turime DNR grandinę užkoduotą tekstu var txt =" T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ".
+        Galimos bazės: Adenine, Thymine, Cytosine, Guanine
+         Parašykite programą kurioje atsiranda MENIU kuriame naudotojas gali pasirinkti:
+         1. Atlikti DNR grandinės normalizavimo veiksmus:
+            - pašalina tarpus.
+            - visas raides keičia didžiosiomis. 
+         2. Atlikti grandinės validaciją
+            - patikrina ar nėra kitų nei ATCG raidžių
+         3. Atlieka veiksmus su DNR grandine (tik tuo atveju jei grandinė yra normalizuota ir validi). Nuspaudus 3 įeinama į sub-meniu
+             - Jeigu grandinė yra validi, tačiau nenormalizuota programa pasiūlo naudotojui 
+             1) normalizuoti grandinę
+             2) išeiti iš programos
+             - jei grandinė normalizuota arba kai buvo atlikta normalizacija
+             1) GCT pakeis į AGG
+             2) Išvesti ar yra tekste CAT 
+             3) Išvesti trečia ir penktą grandinės segmentus (naudoti Substring()).
+             4) Išvesti raidžių kiekį tekste (naudoti string composition)
+             5) Išvesti ar yra tekste ir kiek kartų pasikartoja iš klaviatūros įvestas segmento kodas 
+             6) Prie grandinės galo pridėti iš klaviatūros įvesta segmentą  
+                 (reikalinga validacija ar nėra kitų kaip ATCG ir 3 raidės) 
+             7) Iš grandinės pašalinti pasirinką elementą  
+             8) Pakeisti pasirinkti segmentą įvestu iš klaviatūros  
+                 (reikalinga validacija ar nėra kitų kaip ATCG ir 3 raidės) 
+             9) Gryžti į ankstesnį meniu
+       Visoms operacijoms reikalingi testai.
+         
+         
+         */
     }
 }
