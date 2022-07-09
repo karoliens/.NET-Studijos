74b90a3990c96ad2870755d2c4e15233fbcde281
Karolis <karoliens@gmail.com>
2022-07-10 02:38:29 +0300

    Add working code and tests

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -5,17 +5,51 @@
         static void Main(string[] args)
         {
             var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
-            var normalizuotasTxt = Normalizuoja(txt);
 
             Console.WriteLine($"DNR grandinė {txt}");
             Console.WriteLine("Pasirinkite veiksmą:");
+            PagrindinioMenuSpausdinimas();
+            PagrindinioMenuNavigacija(txt);
+        }
+
+        public static string PasalinaTarpus(string txt)
+        {
+            return txt.Replace(" ", "");
+        }
+
+        public static string KeiciaIDidziasias(string txt)
+        {
+            return txt.ToUpper();
+        }
+
+        public static string Normalizuoja(string txt)
+        {
+            return PasalinaTarpus(KeiciaIDidziasias(txt));
+        }
+
+        public static bool Validuoja(string txt)
+        {
+            var txtTuscias = Normalizuoja(txt).Replace("A", "").Replace("T", "").Replace("C", "").Replace("G", "").Replace("-", "");
+            if (txtTuscias.Length == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void PagrindinioMenuSpausdinimas()
+        {
             Console.WriteLine("1. Atlikti DNR grandinės normalizavimą");
             Console.WriteLine("2. Atlikti DNR grandinės validavimą");
             Console.WriteLine("3. Atlikti veiksmus su DNR grandine");
+        }
 
-            //Console.WriteLine(normalizuotasTxt);
-            //KeiciaGCTiAGG(normalizuotasTxt);
-
+        public static void PagrindinioMenuNavigacija(string txt)
+        {
             switch (Console.ReadLine())
             {
                 case "1":
@@ -35,7 +69,13 @@
                 case "3":
                     if (Validuoja(txt) && !(Normalizuoja(txt) == txt))
                     {
-                        IskvieciaSubMenu1(txt);
+                        SubMenu1Spausdinimas();
+                        SubMenu1Navigacija(txt);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("DNR grandinė nėra validi");
                     }
                     break;
                 default:
@@ -44,50 +84,22 @@
             }
         }
 
-        public static string PasalinaTarpus(string txt)
-        {
-            return txt.Replace(" ", "");
-        }
-
-        public static string KeiciaIDidziasias(string txt)
-        {
-            return txt.ToUpper();
-        }
-
-        public static string Normalizuoja(string txt)
-        {
-            string normalizuotasTxt = PasalinaTarpus(KeiciaIDidziasias(txt));
-
-            return normalizuotasTxt;
-        }
-
-        public static bool Validuoja(string txt)
-        {
-            var txtTuscias = Normalizuoja(txt).Replace("A", "").Replace("T", "").Replace("C", "").Replace("G", "").Replace("-", "");
-            if (txtTuscias.Length == 0)
-            {
-                return true;
-
-            }
-
-            else
-            {
-                return false;
-            }
-        }
-
-        public static void IskvieciaSubMenu1(string txt)
+        public static void SubMenu1Spausdinimas()
         {
             Console.WriteLine("DNR grandinė yra validi, tačiau nenormalizuota");
             Console.WriteLine("Pasirinkite veiksmą:");
             Console.WriteLine("1. Normalizuoti DNR grandinę");
             Console.WriteLine("2. Išeiti iš programos");
+        }
 
+        public static void SubMenu1Navigacija(string txt)
+        {
             switch (Console.ReadLine())
             {
                 case "1":
-                    Console.WriteLine(Normalizuoja(txt));
-                    IskvieciaSubMenu2(txt);
+                    Normalizuoja(txt);
+                    SubMenu2Spausdinimas();
+                    SubMenu2Navigacija(txt);
                     break;
                 case "2":
                     Environment.Exit(0);
@@ -98,7 +110,7 @@
             }
         }
 
-        public static void IskvieciaSubMenu2(string normalizuotasTxt)
+        public static void SubMenu2Spausdinimas()
         {
             Console.WriteLine("DNR grandinė yra normalizuota arba buvo atlikta normalizacija");
             Console.WriteLine("Pasirinkite veiksmą:");
@@ -111,25 +123,139 @@
             Console.WriteLine("7. Iš grandinės pašalinti pasirinką elementą");
             Console.WriteLine("8. Pakeisti pasirinktą segmentą įvestu iš klaviatūros");
             Console.WriteLine("9. Grįžti į ankstesnį meniu");
+        }
 
-            
+        public static void SubMenu2Navigacija(string txt)
+        {
+            string ivestasSegmentas;
+            string pasirinktasSegmentas;
+            string keitimas;
             switch (Console.ReadLine())
             {
                 case "1":
-                    KeiciaGCTiAGG(normalizuotasTxt);
+                    Console.WriteLine($"Normalizuota grandinė: {Normalizuoja(txt)}");
+                    Console.WriteLine($"Pakeista DNR grandinė: {KeiciaGCTiAGG(txt)}");
                     break;
                 case "2":
-                    Environment.Exit(0);
+                    Console.WriteLine($"Normalizuota grandinė: {Normalizuoja(txt)}");
+                    Console.WriteLine($"Pakeista DNR grandinė: {IeskoCAT(txt)}");
+                    break;
+                case "3":
+                    Console.WriteLine($"Normalizuota grandinė: {Normalizuoja(txt)}");
+                    Console.WriteLine($"Pakeista DNR grandinė: {IeskoTrecioIrPenktoSegmento(txt)}");
+                    break;
+                case "4":
+                    Console.WriteLine($"Normalizuota grandinė: {Normalizuoja(txt)}");
+                    Console.WriteLine($"Pakeista DNR grandinė: {IsvedaRaidziuKieki(txt)}");
+                    break;
+                case "5":
+                    Console.WriteLine("Įveskite segmentą:");
+                    ivestasSegmentas = Console.ReadLine();
+                    Console.WriteLine(IeskoIvestoSegmento(txt, ivestasSegmentas));
+                    break;
+                case "6":
+                    Console.WriteLine("Įveskite segmentą:");
+                    ivestasSegmentas = Console.ReadLine();
+                    Console.WriteLine($"Normalizuota grandinė: {Normalizuoja(txt)}");
+                    Console.WriteLine($"Pakeista DNR grandinė: {PridedaIGala(txt, ivestasSegmentas)}");
+                    break;
+                case "7":
+                    Console.WriteLine("Įveskite segmentą:");
+                    ivestasSegmentas = Console.ReadLine();
+                    Console.WriteLine($"Normalizuota grandinė: {Normalizuoja(txt)}");
+                    Console.WriteLine($"Pakeista DNR grandinė: {PasalinaSegmenta(txt, ivestasSegmentas)}");
+                    break;
+                case "8":
+                    Console.WriteLine("Pasirinkite segmentą, kurį norite pakeisti:");
+                    pasirinktasSegmentas = Console.ReadLine();
+                    Console.WriteLine("Pasirinkite segmentą:");
+                    keitimas = Console.ReadLine();
+                    Console.WriteLine($"Normalizuota grandinė: {Normalizuoja(txt)}");
+                    Console.WriteLine($"Pakeista DNR grandinė: {PakeiciaSegmenta(txt, pasirinktasSegmentas, keitimas)}");
+                    break;
+                case "9":
+                    SubMenu1Spausdinimas();
+                    SubMenu1Navigacija(txt);
                     break;
                 default:
-                    Console.WriteLine("Pasirinkite 1 arba 2");
+                    Console.WriteLine("Pasirinkite nuo 1 iki 9");
                     break;
             }
         }
 
-        public static void KeiciaGCTiAGG(string normalizuotasTxt)
+        public static string KeiciaGCTiAGG(string txt)
         {
-            Console.WriteLine(normalizuotasTxt.Replace("GCT", "AGG"));
+            return Normalizuoja(txt).Replace("GCT", "AGG");
         }
-    }
+
+        public static string IeskoCAT(string txt)
+        {
+            if (Normalizuoja(txt).Contains("CAT"))
+            {
+                return "Taip";
+            }
+
+            else
+            {
+                return "Ne";
+            }
+        }
+
+        public static string IeskoTrecioIrPenktoSegmento(string txt)
+        {
+            var treciasSegmentas = Normalizuoja(txt).Replace("-", "").Substring(6, 3);
+            var penktasSegmentas = Normalizuoja(txt).Replace("-", "").Substring(12, 3);
+
+            return $"{treciasSegmentas} {penktasSegmentas}";         
+        }
+
+        public static int IsvedaRaidziuKieki(string txt)
+        {
+            return Normalizuoja(txt).Replace("-", "").Length;
+        }
+
+        public static string IeskoIvestoSegmento(string txt, string ivestasSegmentas)
+        {
+            if (Normalizuoja(txt).Contains(ivestasSegmentas.ToUpper()))
+            {
+                return "Įvestas segmentas yra DNR grandinėje";
+            }
+
+            else
+            {
+                return "Įvesto segmento nėra DNR grandinėje";
+            }
+        }
+
+        public static string PridedaIGala(string txt, string ivestasSegmentas)
+        {
+            if (Validuoja(ivestasSegmentas) && ivestasSegmentas.Length == 3)
+            {
+                return $"{Normalizuoja(txt)}-{ivestasSegmentas.ToUpper()}";
+            }
+
+            else
+            {
+                return "Segmentą turi sudaryt trys raidės ir tik ATCG";
+            }
+        }
+
+        public static string PasalinaSegmenta(string txt, string ivestasSegmentas)
+        {
+            return Normalizuoja(txt).Replace(ivestasSegmentas, "");
+        }
+
+        public static string PakeiciaSegmenta(string txt, string pasirinktasSegmentas, string keitimas)
+        {
+            if ((Validuoja(pasirinktasSegmentas) && pasirinktasSegmentas.Length == 3) && (Validuoja(keitimas) && keitimas.Length == 3))
+            {
+                return Normalizuoja(txt).Replace(pasirinktasSegmentas, keitimas);
+            }
+
+            else
+            {
+                return "Segmentą turi sudaryt trys raidės ir tik ATCG";
+            }
+        }
+    }   
 }
--- Assignment4/Assignment4_Tests/Assignment4_Tests.cs
+++ Assignment4/Assignment4_Tests/Assignment4_Tests.cs
@@ -1,11 +1,145 @@
-namespace Assignment4_Tests
+﻿namespace Assignment4_Tests
 {
     [TestClass]
     public class Assignment4_Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Normalizuoja1()
         {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var expected = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
+            var actual = Assignment4.Program.Normalizuoja(txt);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Validuoja1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var expected = true;
+            var actual = Assignment4.Program.Validuoja(txt);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Validuoja2()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCTl    ";
+            var expected = false;
+            var actual = Assignment4.Program.Validuoja(txt);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void KeiciaGCTiAGG1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var expected = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-AGG";
+            var actual = Assignment4.Program.KeiciaGCTiAGG(txt);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IeskoCAT1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var expected = "Taip";
+            var actual = Assignment4.Program.IeskoCAT(txt);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IeskoTrecioIrPenktoSegmento1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var expected = "GAC CGT";
+            var actual = Assignment4.Program.IeskoTrecioIrPenktoSegmento(txt);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsvedaRaidziuKieki1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var expected = 39;
+            var actual = Assignment4.Program.IsvedaRaidziuKieki(txt);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void IeskoIvestoSegmento1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var ivestasSegmentas = "TAC";
+            var expected = "Įvestas segmentas yra DNR grandinėje";
+            var actual = Assignment4.Program.IeskoIvestoSegmento(txt, ivestasSegmentas);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void PridedaIGala1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var ivestasSegmentas = "ATC";
+            var expected = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT-ATC";
+            var actual = Assignment4.Program.PridedaIGala(txt, ivestasSegmentas);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void PasalinaSegmenta1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var ivestasSegmentas = "TAC";
+            var expected = "TCG--GAC--CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
+            var actual = Assignment4.Program.PasalinaSegmenta(txt, ivestasSegmentas);
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void PakeiciaSegmenta1()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var pasirinktasSegmentas = "AGA";
+            var keitimas = "TAC";
+            var expected = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-TAC-GCT";
+            var actual = Assignment4.Program.PakeiciaSegmenta(txt, pasirinktasSegmentas, keitimas);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PakeiciaSegmenta2()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var pasirinktasSegmentas = "TAH";
+            var keitimas = "AGA";
+            var expected = "Segmentą turi sudaryt trys raidės ir tik ATCG";
+            var actual = Assignment4.Program.PakeiciaSegmenta(txt, pasirinktasSegmentas, keitimas);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PakeiciaSegmenta3()
+        {
+            var txt = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+            var pasirinktasSegmentas = "AGA";
+            var keitimas = "TAH";
+            var expected = "Segmentą turi sudaryt trys raidės ir tik ATCG";
+            var actual = Assignment4.Program.PakeiciaSegmenta(txt, pasirinktasSegmentas, keitimas);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
