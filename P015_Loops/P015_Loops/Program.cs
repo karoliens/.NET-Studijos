2f5efeb6fa98a16e9bf42fe58bc45e59083c5661
Karolis <karoliens@gmail.com>
2022-07-16 20:47:15 +0300

    Add tasks description

---
+++ P015_Loops/P015_Loops/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace P015_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+
+        /*
+        Parašykite metodą SkaiciuEile kuri išvestu vienoje eilutėje skaičių grupes tokiu principu: -> 1 -> 11 -> 111 -> 1111 -> 11111 -> ....... 
+        programa turi paprašyti nurodyti skaičių ir grupių kiekį
+        naudokite for ir StringBuilder
+        */
+
+        /*
+        Sukurkite programa, kuri paprasytu naudotojo ivesti skaiciu. Ivedus skaiciu turetu atspausdinti to skaiciaus daugybos lentele. Pvz
+        15 X 1 = 15                                                                                                   
+        15 X 2 = 30                                                                                                   
+        15 X 3 = 45                                                                                                   
+        15 X 4 = 60                                                                                                   
+        15 X 5 = 75                                                                                                   
+        15 X 6 = 90                                                                                                   
+        15 X 7 = 105                                                                                                  
+        15 X 8 = 120                                                                                                  
+        15 X 9 = 135                                                                                                  
+        15 X 10 = 150
+        */
+
+        /*
+        Sukurkite programa, kuri atspausdintu sia forma:
+
+            *
+           *** 
+          *****
+         *******
+        ********* 
+         *******
+          *****
+           ***
+            * 
+        */
+
+        /*
+        Parasykite programa, kuri atspausdintu sia figura pvz:
+            1 
+            01
+            101 
+            0101 
+            10101
+        */
+
+        /*
+        Parasykite programa, kuri paprasytu ivesti skaiciu ir ivesta skaiciu atspausdintu atvirkstine seka.
+        Naudoti tik ciklus ir matematines operacijas.Visi kintamieji yra integer tipo. Pvz:            Ivedam- 12345 (int)            Rezultatas-54321 (int) 
+        */
+    }
+}
