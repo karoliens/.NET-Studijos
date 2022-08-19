ea8aa8c42859e756a3398f62ecd55201065d2ed4
Karolis <karoliens@gmail.com>
2022-08-19 12:29:30 +0300

    Initial commit

---
+++ OOP/Enum/Enum/Models/Person.cs
@@ -0,0 +1,75 @@
+﻿using Praktika.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika.Models
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Person()
+        {
+
+        }
+
+        public Person(EGenderType gender)
+        {
+            Gender = gender;
+        }
+
+        public int Id { get; set; }
+        public string FirstName 
+        { 
+            get => _firstName; 
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(_firstName))
+                {
+                    NameChanges += $"{_firstName} -> {value} ";
+                }
+                _firstName = value;
+            }
+        }
+        public string LastName 
+        { 
+            get => _lastName;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(_lastName))
+                {
+                    NameChanges += $"{_lastName} -> {value} ";
+                }
+                _lastName = value;
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return $"{FullName} {LastName}";
+            }
+        }
+        public EGenderType Gender { get; set; }
+        public void ChangeGender(EGenderType gender)
+        {
+            Gender = gender;
+        }
+        public DateTime? BirthDate { get; set; }
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public int? Age => GetAge();
+        private int? GetAge()
+        {
+            if (BirthDate == null)
+                return null;
+            var ts = DateTime.Now.Subtract((DateTime)BirthDate);
+            return new DateTime(ts.Ticks).Year - 1;
+        }
+        public string NameChanges { get; private set; }
+    }
+}
---
+++ OOP/Enum/Enum/Models/Society.cs
@@ -0,0 +1,41 @@
+﻿using Praktika.InitialData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika.Models
+{
+    public class Society
+    {
+        public List<Person> People;
+        public void FillPeople()
+        {
+            People = PersonInitialData.DataSeed.ToList();
+        }
+        public List<Person> OldPeople
+        {
+            get
+            {
+                var lst = new List<Person>();
+                foreach(var p in People)
+                {
+                    if (p.BirthDate < new DateTime(2001,1,1))
+                        lst.Add(p);
+                }
+                return lst;
+            }
+        }
+        public List<Person> Men;
+        public List<Person> Women;
+        public void FillMen()
+        {
+            Men = PersonInitialData.DataSeed.ToList();
+        }
+        public void FillWomen()
+        {
+            Women = PersonInitialData.DataSeed.ToList();
+        }
+    }
+}
