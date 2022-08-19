94c6ece4b5c6d09f1cd56ffa773ffc350e57ed1b
Karolis <karoliens@gmail.com>
2022-08-19 22:16:01 +0300

    Revert "Initial commit"
    This reverts commit 54be48d4588bd15ae7cbf2e3a0af312cc19281ee.

--- OOP/NamuDarbai/08_11_NamuDarbas_2/08_11_NamuDarbas_2/Program.cs
+++ OOP/NamuDarbai/08_11_NamuDarbas_2/08_11_NamuDarbas_2/Program.cs
@@ -4,19 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /*
-            2.    Parasykite klase „Skaiciuoklis“, kuris turetu 1 private property pavadinimu Skaicius ir 3 metodus: Didinti (Padidina Skaicius 1), Mazinti(Sumazina Skaicius 1), Atspausdinti()
-            a.   Padarykite, kad Mazinti() metodas negaletu sumazinti <Skaicius> maziau 0
-            b.    Sukurkite metoda Perkrauti() <Reset>, kuris turetu grazinti <Skaicius> i pradine busena (Perkrauti metodas turetu nepriimti jokiu parametru)
-
-            3. Sukurkite „Produktas“ klase, kuri turetu 3 properties su pasleptais seteriais – Kaina, kiekis, pavadinimas. Sukurkite
-            „Produktas“ metoda void SpausdintiProdukta(), kuris atspausdintu informacija apie produkta tokiu formatu „Kivis kaina 1,50$: 3 vnt“
-
-            4. Pasirašyti klasę <SkaiciuKrepselis>, kuris turėtų <private List<int>> skaiciu sarasa. Skaiciu sąrašui užpildyti sukurkite naują metodą <void PridėtiSkaiciu(int skaicius)>. Taip pat sukurkite
-            naują metodą <double ApskaiciuotiVidurki()>, kuris apskaiciuoja visu sarase esanciu skaiciu vidurki.
-
-            5. Parašyti po 1 metoda kiekvienai iš jūsų <Kambarys> namų darbo klasei (~12 klasių). Stenkitės pasipraktuokuoti skirtingus gražinimo duomenų tipus ir gal net vienam ar kitam metodui pridėkite po kokį nors parametrą.Papildomai galite ~6 klasėms sukurti po konstruktorių, kuris kelis jūsų pasirinktus <private set> properties(Siuo metu visi yra public) inicijuotų per kontruktoriu paduotas reiksmes. 
-            */
+            Console.WriteLine("Hello, World!");
         }
     }
 }
