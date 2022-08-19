1ff5aed48966dcbdd2235bd48687bb59917c6022
Karolis <karoliens@gmail.com>
2022-08-19 13:04:27 +0300

    Initial commit

---
+++ OOP/NamuDarbai/08_11_NamuDarbas_2/08_11_NamuDarbas_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08_11_NamuDarbas_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
