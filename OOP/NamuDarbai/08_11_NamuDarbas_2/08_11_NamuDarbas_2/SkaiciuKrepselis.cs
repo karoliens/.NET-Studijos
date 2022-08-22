c69189702248e94b1a1977a4bf5f2ebb0f33f778
Karolis <karoliens@gmail.com>
2022-08-22 12:23:13 +0300

    Add initial solution

---
+++ OOP/NamuDarbai/08_11_NamuDarbas_2/08_11_NamuDarbas_2/SkaiciuKrepselis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_11_NamuDarbas_2
+{
+    internal class SkaiciuKrepselis
+    {
+        private List<int> skaiciuSarasas;
+
+        public void PridėtiSkaiciu(int skaicius)
+        {
+            skaiciuSarasas.Add(skaicius);
+        }
+
+        public double ApskaiciuotiVidurki()
+        {
+            int suma = 0;
+
+            foreach(var skaicius in skaiciuSarasas)
+            {
+                suma = suma + skaicius;
+            }
+
+            return suma / skaiciuSarasas.Count();
+        }
+    }
+}
---
+++ OOP/NamuDarbai/08_11_NamuDarbas_2/08_11_NamuDarbas_2/Skaiciuoklis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_11_NamuDarbas_2
+{
+    internal class Skaiciuoklis
+    {
+        private int pradinisSkaicius; // Sukuriamas, kad būtų galima nustatyti pradinį skaičių paduodant į konstruktorių
+        private int Skaicius { get; set; }
+
+        public Skaiciuoklis()
+        {
+            Skaicius = 0;
+            pradinisSkaicius = 0;
+        }
+
+        public Skaiciuoklis(int skaicius)
+        {
+            Skaicius = skaicius;
+            pradinisSkaicius = skaicius; 
+        }
+
+        public void Didinti()
+        {
+            Skaicius++;
+        }
+
+        public void Mazinti()
+        {
+            Skaicius--;
+        }
+
+        public void Atspausdinti()
+        {
+            Console.WriteLine(Skaicius);
+        }
+
+        public void Perkrauti()
+        {
+            Skaicius = pradinisSkaicius; // pradinisSkaicius reikšmė perduodama Skaicius
+        }
+    }
+}
