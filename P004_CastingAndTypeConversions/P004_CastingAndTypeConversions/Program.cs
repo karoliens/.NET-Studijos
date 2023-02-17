fb00694a7b35791b7bb8d795c72ef491d62d6e0d
Karolis <karoliens@gmail.com>
2023-02-17 14:22:28 +0200

    Revert "Updated solutions"
    This reverts commit 5741ae04a37d7e7c2b7054b12a337420a2098b72.

--- P004_CastingAndTypeConversions/P004_CastingAndTypeConversions/Program.cs
+++ P004_CastingAndTypeConversions/P004_CastingAndTypeConversions/Program.cs
@@ -11,7 +11,7 @@
             • SANDAUGĄ
             • DALYBĄ
             */
-
+            /*
             Console.WriteLine("Įveskite pirmą skaičių:");
             string skaicius1 = Console.ReadLine();
 
@@ -21,10 +21,10 @@
             Console.WriteLine($"Skaičių suma: {Convert.ToInt32(skaicius1) + Convert.ToInt32(skaicius2)}");
             Console.WriteLine($"Skaičių skirtumas: {Convert.ToInt32(skaicius1) - Convert.ToInt32(skaicius2)}");
             Console.WriteLine($"Skaičių sandauga: {Convert.ToInt32(skaicius1) * Convert.ToInt32(skaicius2)}");
-            Console.WriteLine($"Skaičių dalyba: {(double)Convert.ToInt32(skaicius1) / Convert.ToInt32(skaicius2)}");
+            Console.WriteLine($"Skaičių dalyba: {Convert.ToInt32(skaicius1) / Convert.ToInt32(skaicius2)}");
 
             //PARAŠYTI PROGRAMĄ, KURIOJE VARTOTOJO PRAŠOMA ĮVESTI 3 SKAIČIUS. PROGRAMA TURI IŠVESTI ŠIŲ SKAIČIŲ VIDURKĮ.
-
+            
             Console.WriteLine("Įveskite pirmą skaičių:");
             int s1 = int.Parse(Console.ReadLine());
 
@@ -43,13 +43,13 @@
             - iš rezultato atimkite maksimalų long skaičių
             - ir pridėkite maksimalų int skaičių
             */
-
+            /*
             long ilgas = long.MaxValue;
             short trumpas = short.MaxValue;
 
             Console.WriteLine($"Dalyba: {(double)ilgas / trumpas}");
-            Console.WriteLine($"Atimtis: {(double)ilgas / trumpas - ilgas}");
-            Console.WriteLine($"Sudėtis: {(double)ilgas / trumpas + int.MaxValue}");
+
+            */
 
             /*
             PARAŠYTI PROGRAMĄ KURI DIDELĮ 10 ŽENKLĮ SKAIČIŲ DOUBLE, KONVERTUOJA Į
@@ -57,24 +57,31 @@
             STEBĖTI REZULTATĄ.
             */
 
+            /*
+
             double didelisSkaicius = 1000000000;
 
             Console.WriteLine($"INT: {Convert.ToInt32(didelisSkaicius)}");
-            // Console.WriteLine($"SHORT: {Convert.ToInt16(didelisSkaicius)}"); // netelpa
-            // Console.WriteLine($"BYTE: {Convert.ToByte(didelisSkaicius)}"); // netelpa
+            Console.WriteLine($"SHORT: {Convert.ToInt16(didelisSkaicius)}");
+            Console.WriteLine($"BYTE: {Convert.ToByte(didelisSkaicius)}");
+            
+            */
 
             /*
             PARAŠYTI PROGRAMĄ KURI
             PRAŠO ĮVESTI RUTULIO DIAMETRĄ,
             O IŠVEDA PLOTĄ IR TŪRĮ
+
             */
-
+            /*
             Console.WriteLine("Įveskite rutulio diametrą:");
-            var d = double.Parse(Console.ReadLine());
-            var r = d / 2;
+            var d = Console.ReadLine();
 
-            Console.WriteLine($"Plotas: {4 * 3.14 * r * r}");
-            Console.WriteLine($"Tūris: {4 / 3 * 3.14 * r * r * r}");
+
+
+            Console.WriteLine($"Plotas: {4 * 3.14 * Convert.ToInt16(d) * Convert.ToInt16(d)}");
+            Console.WriteLine($"Tūris: {4 / 3 * 3.14 * Convert.ToInt16(d) * Convert.ToInt16(d) * Convert.ToInt16(d)}");
+            */
 
             /*
             PARAŠYTI PROGRAMĄ KURI PRAŠO ĮVESTI ATSTUMĄ (METRAIS) IR LAIKĄ (SEKUNDĖMIS),
@@ -82,6 +89,8 @@
             - IŠVEDA GREITĮ km/s.
             */
 
+
+            /*
             Console.WriteLine("Įveskite atstumą metrais:");
             var atstumas = double.Parse(Console.ReadLine());
 
@@ -91,12 +100,12 @@
             Console.WriteLine($"Greitis km/h: {((atstumas) / 1000) / ((laikas) * 3600)}");
             Console.WriteLine($"Greitis km/s: {((atstumas) / 1000) / (laikas)}");
 
-            /*
-            Nuskaitykite iš klaviatūros 2 skaičius (x ir y).
-            Išveskite į ekraną funkciją y+2y+x+1 ir apskaičiuokite šios funkcijos rezultatą.
-            Išveskite į ekraną funkciją y²+x/2 apskaičiuokite šios funkcijos rezultatą.
             */
 
+            /*
+            
+            */
+            /*
             var x = int.Parse(Console.ReadLine());
             var y = int.Parse(Console.ReadLine());
             var rezultatas1 = y + 2 * y + x + 1;
@@ -104,6 +113,7 @@
 
             Console.WriteLine($"{rezultatas1}");
             Console.WriteLine($"{rezultatas2}");
+            */
 
             /*
             PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI PENKIAŽENKLĮ SVEIKĄ SKAIČIŲ
@@ -113,10 +123,12 @@
             <HINT> naudokite replace
             */
 
+            /*
             Console.WriteLine("Įveskite penkiaženklį sveiką skaičių:");
 
-            int penkiazenklisSkaicius = int.Parse(Console.ReadLine().Replace("1", "0"));
-            Console.WriteLine($"Rezultatas: {penkiazenklisSkaicius}/2={(double)penkiazenklisSkaicius / 2}");
+            int skaicius = int.Parse(Console.ReadLine().Replace("1", "0"));
+            Console.WriteLine($"{skaicius}");
+            */
 
             /*
             PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI PENKIAŽENKLĮ SVEIKĄ SKAIČIŲ
@@ -125,20 +137,10 @@
             REZULTATAS 10000/2=5000
             <HINT> naudokite replace
             */
-
+            /*
             Console.WriteLine("Įveskite penkiaženklį sveiką skaičių:");
-
-            int penkiazenklisSkaicius1 = int.Parse(Console.ReadLine()
-                .Replace("2", "0")
-                .Replace("3", "0")
-                .Replace("4", "0")
-                .Replace("5", "0")
-                .Replace("6", "0")
-                .Replace("7", "0")
-                .Replace("8", "0")
-                .Replace("9", "0"));
-
-            Console.WriteLine($"Rezultatas: {penkiazenklisSkaicius1}/2={(double)penkiazenklisSkaicius1 / 2}");
+            int skaicius = int.Parse(Console.ReadLine().Replace("2", "0", "1"));
+            */
 
             /*
             PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI SVEIKĄ SKAIČIŲ
@@ -149,8 +151,12 @@
 
             Console.WriteLine("Įveskite sveiką skaičių:");
             var skaicius = int.Parse(Console.ReadLine());
+            var skaiciusPadidintas = ++skaicius;
 
-            Console.WriteLine($"{++skaicius} {++skaicius} {++skaicius} {++skaicius} {++skaicius}");
+            Console.WriteLine($"{skaiciusPadidintas}{skaiciusPadidintas+1}{skaiciusPadidintas+2}{skaiciusPadidintas+3}{skaiciusPadidintas+4}");
+
+
+
         }
     }
 }
